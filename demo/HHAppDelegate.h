0331ebcfe8b02735fb651796f2c1314f7ee91c79
czda1100 <czda1100@yahoo.cn>
2012-03-09 22:28:54 +0800

    Initial Commit

---
+++ demo/HHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HHAppDelegate.h
+//  demo
+//
+//  Created by Eric on 12-3-9.
+//  Copyright (c) 2012年 Tian Tian Tai Mei Net Tech (Bei Jing) Lt.d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
